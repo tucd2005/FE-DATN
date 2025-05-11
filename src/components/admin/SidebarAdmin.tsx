58ba99968a3f890d9175238a302c3ee8c98cfb03
tucd2005 <tucdph51409@gmail.com>
2025-05-11 08:39:52 +0700

    base FE

---
+++ src/components/admin/SidebarAdmin.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SidebarAdmin = () => {
+  return (
+    <div>SidebarAdmin</div>
+  )
+}
+
+export default SidebarAdmin
---
+++ src/components/client/HeaderClient.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeaderClient = () => {
+  return (
+    <>
+        <div className='bg-green-400'> [05.09 - 01.10] Càng mua càng giảm, mua 2 giảm 6%, mua 3 giảm 10% </div>
+    </>
+  )
+}
+
+export default HeaderClient
