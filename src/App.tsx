3231c95392148754016c6d6d29c695b28701e5d2
viet261205 <viet22331111@gmail.com>
2025-05-14 17:10:13 +0700

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-
 import { useRoutes } from 'react-router-dom'
 import HomeAdmin from './pages/admin/adminhome'
-
 import EditSanPham from './pages/admin/Edit_san_pham'
 import HomeClient from './pages/client/home'
 import AddSanPham from './pages/admin/Add_san_pham'
@@ -9,9 +7,6 @@ import QuanLySanPham from './pages/admin/quan_ly_san_pham'
 import QuanLyDanhMuc from './pages/admin/quan_ly_danh_muc'
 import AddDanhMuc from './pages/admin/add_danh_muc'
 import EditDanhMuc from './pages/admin/edit_danh_muc'
-
-
-
 function App() {
   const router=useRoutes([
     {
