183fb19b70f5a19ebf321ff99e7e1f429a634aac
tucd2005 <tucdph51409@gmail.com>
2025-05-14 10:52:14 +0700

    tu update quan-li-banner

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,6 @@ import HomeAdmin from './pages/admin/adminhome'
 import EditSanPham from './pages/admin/Edit_san_pham'
 import HomeClient from './pages/client/home'
 import AddSanPham from './pages/admin/Add_san_pham'
-import QuanLySanPham from './pages/admin/quan_ly_san_pham'
-import QuanLyDanhMuc from './pages/admin/quan_ly_danh_muc'
-import AddDanhMuc from './pages/admin/add_danh_muc'
-import EditDanhMuc from './pages/admin/edit_danh_muc'
 
 
 
@@ -29,18 +25,6 @@ function App() {
     {
           path: "/admin/add-san-pham",
           element: < AddSanPham/>,
-    },
-    {
-          path: "/admin/add-danh-muc",
-          element: < AddDanhMuc/>,
-    },
-    {
-          path: "/admin/quan-ly-danh-muc",
-          element: < QuanLyDanhMuc/>,
-    },
-    {
-          path: "/admin/edit-danh-muc",
-          element: < EditDanhMuc/>,
     },
 
 
