ee2dbd426120fe8fa03f1b9652b492730b3806e7
viet261205 <viet22331111@gmail.com>
2025-07-03 12:51:12 +0700

    sửa phần sản phẩm

---
+++ src/services/checkoutService.ts
@@ -0,0 +1,20 @@
+
+import instanceAxios from "../utils/axios";
+
+export interface CreateOrderPayload {
+  dia_chi: string;
+  phuong_thuc_thanh_toan: string;
+  items: {
+    san_pham_id: number;
+    bien_the_id: number;
+    so_luong: number;
+  }[];
+}
+
+export const checkoutService = {
+  create: async (data: any) => {
+    const res = await instanceAxios.post("/orders", data);
+    return res.data;
+  },
+};
+
