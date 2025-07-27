2b4d896f5dcd320c89828400e33440c96fc5ee8a
tucd2005 <tucdph51409@gmail.com>
2025-07-27 22:15:30 +0700

    xóa icon auth

--- src/pages/admin/auth/AdminLogin.tsx
+++ src/pages/admin/auth/AdminLogin.tsx
@@ -128,7 +128,6 @@ export default function AdminLogin() {
                 </label>
                 <div className="relative">
                   <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                    <UserOutlined className="h-5 w-5 text-gray-400" />
                   </div>
                   <input
                     type="email"
@@ -151,7 +150,6 @@ export default function AdminLogin() {
                 </label>
                 <div className="relative">
                   <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                    <LockOutlined className="h-5 w-5 text-gray-400" />
                   </div>
                   <input
                     type="password"
