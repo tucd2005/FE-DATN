0f29902db6de80b5534afae1ed87fa7e753386eb
N_HA <hatpph50768@gmail.com>
2025-08-01 22:11:58 +0700

    hien thi danh muc home client

--- src/pages/client/home/components/danh_muc.tsx
+++ src/pages/client/home/components/danh_muc.tsx
@@ -1,15 +1,32 @@
-import React from "react";
-import { Shirt, ShoppingBag, Backpack, Tag, Award, Glasses } from "lucide-react";
+import { Carousel } from "antd";
+import instanceAxios from "../../../../utils/axios";
+import { useEffect, useState } from "react";
+
+type TCategory = {
+  id: number,
+  ten: string,
+  mo_ta: string,
+  image: string,
+  deleted_at: string | null
+  created_at: string | null
+  updated_at: string | null
+}
 
 export default function DanhMuc() {
-  const categories = [
-    { icon: Shirt, name: "Áo thể thao", count: "450 sản phẩm", color: "bg-blue-100 text-blue-600" },
-    { icon: ShoppingBag, name: "Giày thể thao", count: "320 sản phẩm", color: "bg-green-100 text-green-600" },
-    { icon: Backpack, name: "Balo & túi", count: "150 sản phẩm", color: "bg-yellow-100 text-yellow-600" },
-    { icon: Glasses, name: "Kính thể thao", count: "120 sản phẩm", color: "bg-red-100 text-red-600" },
-    { icon: Tag, name: "Phụ kiện", count: "180 sản phẩm", color: "bg-purple-100 text-purple-600" },
-    { icon: Award, name: "Thời trang top", count: "90 sản phẩm", color: "bg-orange-100 text-orange-600" },
-  ];
+  const [cate, setCate] = useState<TCategory[]>([]);
+
+  useEffect(() => {
+    const getCates = async () => {
+      try {
+        const { data } = await instanceAxios.get("/categories");
+        console.log("Fetched categories:", data);
+        setCate(data?.data.data);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    }
+    getCates()
+  }, []);
 
   return (
     <section data-aos="fade-up" className="py-16">
@@ -18,19 +35,16 @@ export default function DanhMuc() {
           <h2 className="text-3xl font-bold text-gray-900 mb-4">Danh mục thể thao</h2>
           <p className="text-gray-600">Khám phá các danh mục đồ thể thao đa dạng cho mọi hoạt động và phong cách</p>
         </div>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-6">
-          {categories.map((category, index) => (
-            <div key={index} className="bg-white rounded-lg border border-gray-200 shadow-sm hover:shadow-lg transition-shadow cursor-pointer">
-              <div className="p-6 text-center">
-                <div className={`w-12 h-12 ${category.color} rounded-lg flex items-center justify-center mx-auto mb-4`}>
-                  <category.icon className="w-6 h-6" />
-                </div>
-                <h3 className="font-semibold text-gray-900 mb-1">{category.name}</h3>
-                <p className="text-sm text-gray-500">{category.count}</p>
+        <Carousel arrows infinite slidesToShow={5} autoplay autoplaySpeed={3000} swipeToSlide={true}>
+          {cate?.map((e, index) => (
+            <div key={e.ten + index} className="px-3">
+              <div className="flex items-center rounded-[16px] p-2 border border-gray-200 shadow-sm hover:shadow-lg transition-shadow cursor-pointer">
+                <img src={e.image} alt={e.ten} className="h-16 aspect-square object-cover rounded-lg" />
+                <span className="font-semibold mx-auto"> {e.ten}</span>
               </div>
             </div>
           ))}
-        </div>
+        </Carousel>
       </div>
     </section>
   );
