fa279b9ff278d4beb0bfbc1e39d519b342ef73d9
phuongha-alt <hatpph50768@gmail.com>
2025-08-05 22:24:04 +0700

    chore: thêm font chữ poppin cho dự án

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+    plugins: [],
+  }
 }
